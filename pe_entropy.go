bbeca0155246ebe45b3f0776746e20fa09d235c3
Core|Threat <30553333+ipcis@users.noreply.github.com>
2023-10-07 14:19:44 +0200

    Create pe_entropy.go

---
+++ pe_entropy.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"debug/pe"
+	"fmt"
+	"log"
+	"os"
+)
+
+func calculateEntropy(data []byte) float64 {
+	entropy := 0.0
+	totalBytes := float64(len(data))
+
+	frequency := make(map[byte]float64)
+	for _, b := range data {
+		frequency[b]++
+	}
+
+	for _, count := range frequency {
+		probability := count / totalBytes
+		entropy -= probability * math.Log2(probability)
+	}
+
+	return entropy
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run entropy_segments.go <PE_file_path>")
+		os.Exit(1)
+	}
+
+	peFilePath := os.Args[1]
+
+	file, err := os.Open(peFilePath)
+	if err != nil {
+		log.Fatalf("Error opening file: %v\n", err)
+	}
+	defer file.Close()
+
+	peFile, err := pe.NewFile(file)
+	if err != nil {
+		log.Fatalf("Error parsing PE file: %v\n", err)
+	}
+
+	for _, section := range peFile.Sections {
+		fmt.Printf("Section: %s\n", section.Name)
+		sectionData, err := section.Data()
+		if err != nil {
+			log.Fatalf("Error reading section data: %v\n", err)
+		}
+
+		entropy := calculateEntropy(sectionData)
+		fmt.Printf("Entropy: %f\n", entropy)
+	}
+}
